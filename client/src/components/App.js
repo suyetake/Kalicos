edb4c80556a35788d9ff349428f74d1550941be6
Christopher Codrington <chcodrington@gmail.com>
2017-09-23 15:38:18 -0600

    add client boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+
+
+const Header = () => (
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/maps">Maps</Link></li>
+    </ul>
+  </div>
+)
+
+const Home = () => (
+  <div> This is the landing </div>
+)
+
+const About = () => (
+  <div> This is the about page </div>
+)
+
+const Maps = () => (
+  <div> This is the maps page </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <Header />
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />
+          <Route path="/maps" component={ Maps } />
+        </div>
+    )
+  }
+}
+
+export default App;
