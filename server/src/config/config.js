571493f56e0faec711537fcfaf60a9dce2e175b5
Fred Wehle <fredmon3@gmail.com>
2018-09-05 19:02:50 -0600

    Cleanup dev setup

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,11 +1,8 @@
 const env = process.env.NODE_ENV || 'development'
 
 var config = {}
-if(env === 'test') {
+if(env === 'test' || env === 'development') {
   config = require('./config.json')
-}
-
-if(env === 'test') {
   const envConfig = config[env]
 
   // eslint-disable-next-line
